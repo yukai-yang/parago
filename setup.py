e5ea2e17dab94e4b3a53b28b1a72e9d389432b0f
凱(Kai) <yukai.yang@gmail.com>
2019-04-17 01:11:30 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 
 setup(name='parago',
-      version='0.1',
+      version='0.2',
       description='implementing the Go routing and R apply like parallel computation',
       long_description='The package implements two different kinds of parallel computation. One mimics the Go routine like concurrent computation, and the other the R apply like parallel computation. They are featured in the way that the user of the package does not need to know how many cores are deployed and gets the jobs done.',
       classifiers=[
